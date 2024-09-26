0265e78fd6d44accfd7cd9a8fc4f5323efa85986
gAlvim <ledyzito@hotmail.com>
2024-09-26 13:12:25 -0300

    add main e outras coisas

--- insta_accounts_api.py
+++ insta_accounts_api.py
@@ -2,22 +2,23 @@ from instagrapi import Client
 from time import sleep
 
 
-class InstaAccounts:
+class InstaAccountsAPI:
     def __init__(self,login,password) -> None:
         self.login = login
         self.password = password
+        self.account = Client()
         
-    def login_and_post(self):
-        account = Client()
+    def login(self):
+        print('Estou tentando logar sem a dupla verificação!...')
+        self.account.login(self.login,self.password)
+        sleep(5)
+        user_info = self.account.user_info()
+        print('Consegui logar na conta:',user_info)
         
-        try:
-            account.login(self.login,self.password)
-            sleep(5)
-            user_info = account.account_info()
-            print('Consegui logar na conta:',user_info.username)
-            
-        except Exception as e:
-            print('Retire a dupla verificação!')
+    def post(self):
+        ...
+    
+        
             
 
         
@@ -25,5 +26,5 @@ class InstaAccounts:
 
             
 if __name__ =='__main__':
-    login = InstaAccounts('username','password!')
-    login.login_and_post()
+    login = InstaAccountsAPI('username','password!')
+    login.login()
---
+++ main.py
@@ -0,0 +1,15 @@
+from insta_accounts_api import InstaAccountsAPI
+from insta_accounts_selenium import InstaAccountSelenium
+
+
+if __name__ == '__main__':
+    try:
+        account_api = InstaAccountsAPI('login','password')
+        account_api.login()
+    except:
+        account_selenium = InstaAccountSelenium('login','password')
+        account_selenium.login()
+        account_selenium.wait_verification_upload()
+        account_selenium.upload_image(r"D:\teste_automacao.png")
+        account_selenium.image_data('Isto é um teste! Por favor, não interaja!')
+        account_selenium.share()
