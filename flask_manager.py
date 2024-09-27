c319a4beb8a96717d8b6c8b14f81a02d0b156f62
gAlvim <ledyzito@hotmail.com>
2024-09-27 15:19:23 -0300

    Add funções do Json

--- flask_manager.py
+++ flask_manager.py
@@ -1,15 +1,23 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
+import os
+from json_import import JsonImport
 
 app = Flask(__name__)
 
 @app.route('/')
 def principal():
-     return render_template('base.html')
+    return render_template('base.html')
 
-
-@app.route('/login',methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     login = request.form['login']
-    password = request.form['senha']
+    senha = request.form['senha']
+    nome_arquivo = request.form['nome_arquivo']
+    json_import = JsonImport()
+    if os.path.exists(nome_arquivo):
+        with open(nome_arquivo, 'a') as file:
+            json_import.add_account(login,senha)
+    json_import.add_account(login, senha)
+    json_import.save_json(nome_arquivo)
     
-    
+    return f"Conta adicionada ao JSON: {nome_arquivo}"
